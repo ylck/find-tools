728fbcbae5bf8d0b86b9e10a94d4297b2ead3ff0
ylck <ylck77@gmail.com>
2018-08-02 07:39:38 +0800

    update

--- cmd/app.go
+++ cmd/app.go
@@ -1,41 +1,46 @@
-package app
+package cmd
 
 import (
-		"github.com/spf13/cobra"
-	"github.com/termie/go-shutil"
 	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"github.com/termie/go-shutil"
 	"os"
 	"path/filepath"
 	"strings"
 )
-var src string
 
-func Run() {
+var src, tar, suffix string
 
-	var cmdPrint = &cobra.Command{
-		Use:   "print [string to print]",
-		Short: "Print anything to the screen",
-		Long: `print is for printing anything back to the screen.
-For many years people have printed back to the screen.`,
-		Args: cobra.MinimumNArgs(1),
-
-		//Run: func(cmd *cobra.Command, args []string) {
-		//	fmt.Println("Print: " + strings.Join(args, " "))
-		//},
-		Run: func(cmd *cobra.Command, args []string) {
-			//copyfile("C:\\code\\src\\user","C:\test")
-			src := args[0]
-			log.Info(src)
-		},
-	}
-
-	var rootCmd = &cobra.Command{Use: "app111"}
-	rootCmd.AddCommand(cmdPrint)
-	rootCmd.Execute()
-	log.Info(src)
+var RootCmd = &cobra.Command{
+	Use: "app",
 }
 
-func copyfile( src_dir,tar_dir string  ) {
+var cmdCopy = &cobra.Command{
+	Use:   "copy [string to echo]",
+	Short: "Echo anything to the screen",
+	Long: `echo is for echoing anything back.
+Echo works a lot like print, except it has a child command.`,
+	//Args: cobra.MinimumNArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		copyfile(src, tar, suffix)
+	},
+}
+
+func init() {
+	RootCmd.PersistentFlags().StringVar(&src, "src", "C:\\code\\src\\user\\cmd", "Author name for copyright attribution")
+	RootCmd.PersistentFlags().StringVar(&tar, "tar", "C:\\test", "Author name for copyright attribution")
+	RootCmd.PersistentFlags().StringVar(&tar, "suffix", ".g", "Author name for copyright attribution")
+
+	// 两个顶层的命令，和一个cmdEcho命令下的子命令cmdTimes
+	RootCmd.AddCommand(cmdCopy)
+
+}
+
+func Run() {
+	RootCmd.Execute()
+}
+
+func copyfile(src_dir, tar_dir, suffix string) {
 	var listfile []string
 	//os.Getenv("dir")
 	filepath.Walk(src_dir, func(path string, info os.FileInfo, err error) error {
@@ -44,7 +49,7 @@ func copyfile( src_dir,tar_dir string  ) {
 		}
 
 		//fmt.Println("file:", info.Name(), "in directory:", path)
-		ok := strings.HasSuffix(path, ".go")
+		ok := strings.HasSuffix(path, suffix)
 		if ok {
 			listfile = append(listfile, path)
 			println("Golang file", path)
@@ -55,11 +60,10 @@ func copyfile( src_dir,tar_dir string  ) {
 	for _, a := range listfile {
 		log.Info(a)
 		new_filename := filepath.Base(a)
-		err := shutil.CopyFile(string(a), tar_dir+new_filename, false)
+		err := shutil.CopyFile(string(a), tar_dir+"\\"+new_filename, false)
 		if err != nil {
 			log.Error(err)
 		}
 	}
-
-	println(listfile[0])
+	log.Info(listfile[0])
 }
